65caf40b1d5181df9d66477ae9b6f4cd29c26f61
pedro <pbmelo@protonmail.com>
2022-02-07 17:38:22 -0700

    making flags explicit

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+# I suggest using something like
 #nmuserhome=$HOME"/Documents/docker/nemaktis/programs/"
 if [-z $1 ]
 then
 	nmuserhome=$1
 	sudo docker run --rm -it \
-		-e DISPLAY=${DISPLAY} \
+		--env DISPLAY=${DISPLAY} \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix\
 		--volume $nmuserhome:/home/nm-user/programs \
 		nemaktis-docker xterm
 else
 	sudo docker run --rm -it \
-		-e DISPLAY=${DISPLAY} \
+		--env DISPLAY=${DISPLAY} \
 		--volume /tmp/.X11-unix:/tmp/.X11-unix \
 		nemaktis-docker xterm
 fi
